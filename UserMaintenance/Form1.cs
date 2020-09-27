396e99b2c90beea44f7fc6769d3ecaf5330ddb57
Dániel Csekő <daniel.cseko@stud.uni-corvinus.hu>
2020-09-27 21:08:22 +0200

    File Save with dialog

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 using UserMaintenance.Entities;
 
 namespace UserMaintenance
@@ -19,6 +21,7 @@ namespace UserMaintenance
             InitializeComponent();
             label2.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.FileSave;
 
             //listbox
             listBox1.DataSource = users;
@@ -40,6 +43,24 @@ namespace UserMaintenance
                 u.FullName = textBox2.Text;
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog svf = new SaveFileDialog();
+            svf.FileName = "Mentett nevek";
+            svf.DefaultExt = ".text";
+            svf.Filter = "Text documents (.txt)|*.txt";
+            if(svf.ShowDialog()== DialogResult.OK)
+            {
+                foreach (var item in users)
+                {
+                    File.AppendAllText(svf.FileName, item.ID.ToString() + " " + item.FullName.ToString()+"\n");
+                }
+            
+            
+            }
+
         }
     }
 }
