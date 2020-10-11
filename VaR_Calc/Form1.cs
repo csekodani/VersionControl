5a2212b35a72bae8b1f9ffc391863ec98c29a899
Dániel Csekő <daniel.cseko@stud.uni-corvinus.hu>
2020-10-11 19:00:06 +0200

    portfolio initialized

--- VaR_Calc/Form1.cs
+++ VaR_Calc/Form1.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VaR_Calc.Entities;
 
 namespace VaR_Calc
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio() 
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+            dataGridView2.DataSource = Portfolio;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
