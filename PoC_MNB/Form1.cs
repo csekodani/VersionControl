13c3b4d1982a9fca6cf78edb9850d77e249993fe
Dániel Csekő <daniel.cseko@stud.uni-corvinus.hu>
2020-10-17 15:57:18 +0200

    cahrd data initialized in genChart

--- PoC_MNB/Form1.cs
+++ PoC_MNB/Form1.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace PoC_MNB
@@ -23,9 +24,11 @@ namespace PoC_MNB
         public Form1()
         {
             InitializeComponent();
-            dataGridView1.DataSource = Rates;  
+            dataGridView1.DataSource = Rates;
+            chartRateData.DataSource = Rates;
             mainResult=getResult();
-            XmlData();     
+            XmlData();
+            genChart();
         }
         private string getResult()
         {
@@ -62,6 +65,25 @@ namespace PoC_MNB
                     rate.Value = value/unit;
                 }
             }
+
+
+        }
+
+        private void genChart()
+        {
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
 
 
         }
