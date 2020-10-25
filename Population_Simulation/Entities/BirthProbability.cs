3c434243c884f871de4fbcc02f0be31a7436f12d
Dániel Csekő <daniel.cseko@stud.uni-corvinus.hu>
2020-10-25 14:35:54 +0100

    Classes are made

---
+++ Population_Simulation/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Population_Simulation.Entities
+{
+    public class BirthProbability
+    {
+
+        public int Age { get; set; }
+        public int NumOfChildren { get; set; }
+        public double Birdt_Probability { get; set; }
+    }
+}
---
+++ Population_Simulation/Entities/DeathProbability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Population_Simulation.Entities
+{
+    public class DeathProbability
+    {
+
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+
+        public double Death_Probability { get; set; }
+
+    }
+}
