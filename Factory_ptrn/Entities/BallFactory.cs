881e0ae597943c3df1da2688c8b90623228dafa0
Dániel Csekő <daniel.cseko@stud.uni-corvinus.hu>
2020-11-15 16:22:51 +0100

    BallFactory class created with createnew function

---
+++ Factory_ptrn/Entities/BallFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_ptrn.Entities
+{
+    class BallFactory
+    {
+        public BallFactory()
+        {
+
+        }
+        public Ball CreateNew()
+        {
+
+            return new Ball();
+        }
+    }
+}
