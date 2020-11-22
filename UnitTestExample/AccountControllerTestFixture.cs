357411f24f8c701050c286c0f93bdafb501268a9
Dániel Csekő <daniel.cseko@stud.uni-corvinus.hu>
2020-11-22 15:02:25 +0100

    3A initiated validation to email

--- UnitTestExample/AccountControllerTestFixture.cs
+++ UnitTestExample/AccountControllerTestFixture.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample
 {
@@ -12,7 +13,12 @@ namespace UnitTestExample
         [Test]
         public void TestValidateEmail(string email, bool expectedResult)
         {
-            
+            //Arrange
+            var AccountController = new AccountController();
+            //Act
+            var actualResult = AccountController.ValidateEmail(email);
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
 
         }
     }
