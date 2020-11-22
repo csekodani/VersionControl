30a0e254656bbb7a749e2e614208224ffe398687
Dániel Csekő <daniel.cseko@stud.uni-corvinus.hu>
2020-11-22 15:25:29 +0100

    password  validation corrected

--- UnitTestExample/Controllers/AccountController.cs
+++ UnitTestExample/Controllers/AccountController.cs
@@ -50,6 +50,18 @@ namespace UnitTestExample.Controllers
 
         public bool ValidatePassword(string password)
         {
+            if (!Regex.IsMatch(password,@"[a-z]"))
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(password, @"[A-Z]"))
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(password, @"[0-9]"))
+            {
+                return false;
+            }
             return true;
         }
     }
