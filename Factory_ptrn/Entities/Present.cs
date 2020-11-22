04ef5758f8e9ebefbabc3bb4ba5ae634882f9350
Dániel Csekő <daniel.cseko@stud.uni-corvinus.hu>
2020-11-22 14:22:18 +0100

    week09 first coomit

---
+++ Factory_ptrn/Entities/Present.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_ptrn.Entities
+{
+    public class Present : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            
+        }
+    }
+}
