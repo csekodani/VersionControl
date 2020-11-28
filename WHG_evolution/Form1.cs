d80e80e5000a51ed454cb3a200cd39b6b2872077
Dániel Csekő <daniel.cseko@stud.uni-corvinus.hu>
2020-11-28 20:22:31 +0100

    button event

--- WHG_evolution/Form1.cs
+++ WHG_evolution/Form1.cs
@@ -85,5 +85,14 @@ namespace WHG_evolution
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
+        }
     }
 }
